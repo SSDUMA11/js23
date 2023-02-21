1047555283429381f67f09ee6451ad18cbf1fc3d
SSDUMA11 <dumasofia2@gmail.com>
2023-02-21 17:38:12 +0200

    task_4

--- task_2.js
+++ task_2.js
@@ -1,6 +1,22 @@
 let a= [5,3,-4,25,32,-16,6]
 let b= [21,-30,9,5,12,-19,5,25]
 
-for (let i = 0; i < a.length; i++){
-    console.log(colors[i]);
+function comparison (){
+    if(a. length > b. length){
+       return 'a > b'; 
+    }
+    else if (a. length < b. length){
+        return 'a < b';
+    }
+    else if (a. length === b. length){
+        return 'a == b';
+    }
 }
+
+console.log(comparison ());
+a[2] = 44;
+a[5] = 44;
+a[7] = 44;
+console.log(comparison ());
+b.length = 2;
+console.log(comparison ());
--- task_4.js
+++ task_4.js
@@ -1,2 +1,8 @@
-let a= [5,3,8,5,3,2,1,2]
-let b= []
+let a= [5,3,8,5,3,2,1,2];
+let b= [];
+a.forEach(function(item){
+    if( b.indexOf(item) === -1) {
+       b.push(item);
+    } 
+})
+console.log(b);
