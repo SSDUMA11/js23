8338ac606ed10176ee989a253fe474380269d849
SSDUMA11 <dumasofia2@gmail.com>
2023-02-24 09:23:53 +0200

    task_2

--- task_2.js
+++ task_2.js
@@ -1,22 +1,26 @@
 let a= [5,3,-4,25,32,-16,6]
 let b= [21,-30,9,5,12,-19,5,25]
 
+function sumArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+}
+ let sumA = sumArray(a);
+ let sumB = sumArray(b);
+
 function comparison (){
-    if(a. length > b. length){
+    if(sumA > sumB){
        return 'a > b'; 
     }
-    else if (a. length < b. length){
+    else if (sumA < sumB){
         return 'a < b';
     }
-    else if (a. length === b. length){
+    else if (sumA === sumB){
         return 'a == b';
     }
 }
+console.log(comparison ());
 
-console.log(comparison ());
-a[2] = 44;
-a[5] = 44;
-a[7] = 44;
-console.log(comparison ());
-b.length = 2;
-console.log(comparison ());
